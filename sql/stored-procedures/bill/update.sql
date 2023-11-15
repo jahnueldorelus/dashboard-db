9cca47e05efa0e2a2d4c75aff8a41b0f1cf2a534
jahnueldorelus <jahnueldorelus@gmail.com>
2023-11-15 09:07:12 -0500

    Fixed mispelling of table in stored procedure 	- The stored procedure to update a bill failed due to the 	  misspelling of the table "Bill".

--- sql/stored-procedures/bill/update.sql
+++ sql/stored-procedures/bill/update.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE `update_user_bill` (IN param_userId INT UNSIGNED, param_billId 
 									param_amount DECIMAL(10,2), param_website VARCHAR(155), 
 									param_dueDate DATE)
 BEGIN
-	UPDATE BILL 
+	UPDATE Bill 
 		SET occurence_id = param_occurenceId, occurs_every = param_occursEvery, recurring = param_recurring, 
 		company = param_company, amount = param_amount, website = param_website, due_date = param_dueDate
 		WHERE user_id = param_userId AND id = param_billId;
