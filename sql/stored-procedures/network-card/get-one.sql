e6367fd35c0b0efaedececb2f299defb7ca7bcaf
jahnueldorelus <jahnueldorelus@gmail.com>
2023-12-14 12:57:23 -0500

    Fixed syntax in stored procedure to retrieve a single virtual machine's network card where retrieving the card resulted in its JSON format to have the incorrect id property "vm_id" instead of "vmId".

--- sql/stored-procedures/network-card/get-one.sql
+++ sql/stored-procedures/network-card/get-one.sql
@@ -15,7 +15,7 @@ BEGIN
 
 	-- Retrieves a network card of a virtual machine
 	ELSE
-		SELECT NetworkCard.id, NetworkCard.vm_id, NetworkCard.server_id AS serverId, NetworkCard.name, 
+		SELECT NetworkCard.id, NetworkCard.vm_id AS vmId, NetworkCard.server_id AS serverId, NetworkCard.name, 
 				NetworkCard.ipv4, NetworkCard.ipv4_subnet as ipv4Subnet,
 				NetworkCard.ipv6, NetworkCard.ipv6_subnet AS ipv6Subnet,
 				NetworkCard.vlan_id AS vlanId, NetworkCard.mac_address AS macAddress
