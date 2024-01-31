8ecb3a81f65e2616d5f74805d2a922be0cbf6564
jahnueldorelus <jahnueldorelus@gmail.com>
2024-01-30 20:27:14 -0500

    Updated database ERD and added trigger for updating a network switch 	- Updated the database's ERD to reflect changes made to entities such as the network switch. 	- Added a trigger to prevent a user from updating a network switch as VLAN incapable when the network switch already have VLANs registered to it.

--- sql/triggers/network-switch/update.sql
+++ sql/triggers/network-switch/update.sql
@@ -7,6 +7,7 @@ CREATE trigger `update_network_switch` BEFORE UPDATE ON NetworkSwitch
 		BEGIN
 			SET @highestPortNumberInUse = (SELECT MAX(port_number) FROM NetworkSwitchPort WHERE switch_id = NEW.id);
 			SET @numOfUsedPorts = (SELECT COUNT(id) FROM NetworkSwitchPort WHERE switch_id = NEW.id);
+			SET @numOfVlansOnSwitch = (SELECT COUNT(id) FROM NetworkSwitchVlan WHERE switch_id = NEW.id);
 			
 			-- Makes sure the new number of ports isn't less than the current amount of ports that exists
 			IF NEW.num_of_ports < @numOfUsedPorts THEN
@@ -17,6 +18,11 @@ CREATE trigger `update_network_switch` BEFORE UPDATE ON NetworkSwitch
 			IF NEW.num_of_ports < @highestPortNumberInUse THEN
 				SET @errorMessage = CONCAT("Cannot update the number of ports to less than what's currently in use. The highest port in use is Port #", @highestPortNumberInUse);
 				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @errorMessage;
+			END IF;
+
+			-- Makes sure the network switch is vlan capable if it has any registered vlans
+			IF OLD.vlan_capable = 1 AND NEW.vlan_capable = 0 AND @numOfVlansOnSwitch > 0 THEN
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Cannot update the network switch as incapable of VLANs when there are VLANs registered to it";
 			END IF;
 		END$$
 DELIMITER ;
